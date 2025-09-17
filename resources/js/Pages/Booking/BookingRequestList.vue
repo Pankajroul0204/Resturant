76d458d09e75c9cb79bc28c7fa15c7b1943ec7cd
Dev@Cyber3 <cymationdeveloper@gmail.com>
2025-09-17 22:52:41 +0530

    payment list

--- resources/js/Pages/Booking/BookingRequestList.vue
+++ resources/js/Pages/Booking/BookingRequestList.vue
@@ -254,10 +254,13 @@ function search() {
 
                                                         </td>
                                                         <td class="px-6 py-4">
-                                                            <span v-if="record.status == 1"
+                                                            <span v-if="record.status == 'confirmed'"
                                                                 class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{{
                                                                     record.status }}</span>
-                                                            <span v-else
+                                                            <span v-else-if="record.status == 'pending'"
+                                                                class="bg-red-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">{{
+                                                                    record.status }}</span>
+                                                            <span v-else-if="record.status == 'cancelled'"
                                                                 class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">{{
                                                                     record.status }}</span>
 
