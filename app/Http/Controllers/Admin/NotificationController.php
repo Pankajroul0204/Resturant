d6c774969bf2481617fc0bc2a7b3f68acbf0a477
Dev@Cyber3 <cymationdeveloper@gmail.com>
2025-09-29 00:08:32 +0530

    notification work

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class NotificationController extends Controller
 {
@@ -14,5 +15,34 @@ class NotificationController extends Controller
         $user = User::find($id);
         $notification_count = $user->unreadNotifications->count();
         return response()->json(['count' => $notification_count]);
+    }
+
+
+    public function markAsRead(Request $request)
+    {
+        $id = auth()->user()->id;
+        $user = User::find($id);
+        $user->unreadNotifications->markAsRead();
+        return response()->json(['status' => 'success']);
+    }
+
+    public function getNotifications()
+    {
+        $id = auth()->user()->id;
+        $user = User::find($id);
+        $todayNotifications = $user->notifications()
+            ->whereDate('created_at', now()->toDateString())
+            ->orderBy('created_at', 'desc')
+            ->get();
+        $yesterdayNotifications = $user->notifications()
+            ->whereDate('created_at', now()->subDay()->toDateString())
+            ->orderBy('created_at', 'desc')
+            ->get();
+        return Inertia::render('Admin/Notification/List', [
+            'notifications' => [
+                'today' => $todayNotifications,
+                'yesterday' => $yesterdayNotifications,
+            ],
+        ]);
     }
 }
